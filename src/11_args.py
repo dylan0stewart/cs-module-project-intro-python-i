15347ca13d8143aa6a02f6ad3c85363034f4552e
dylan0stewart <57077858+dylan0stewart@users.noreply.github.com>
2020-08-11 16:23:04 -0400

    updates

--- src/11_args.py
+++ src/11_args.py
@@ -3,7 +3,8 @@
 
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
-
+def f1(int1, int2):
+    print(int1 + int2)
 # Your code here
 
 
@@ -18,16 +19,16 @@ print(f1(1, 2))
 
 
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
-print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+# print(f2(1))                    # Should print 1
+# print(f2(1, 3))                 # Should print 4
+# print(f2(1, 4, -12))            # Should print -7
+# print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
-a = [7, 6, 5, 4]
+# a = [7, 6, 5, 4]
 
-# What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
-# Your code here
+# # What thing do you have to add to make this work?
+# print(f2(a))    # Should print 22
+# # Your code here
 
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
@@ -39,39 +40,39 @@ print(f2(a))    # Should print 22
 
 
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+# print(f3(1, 2))  # Should print 3
+# print(f3(8))     # Should print 9
 
 
-# Write a function f4 that accepts an arbitrary number of keyword arguments and
-# prints out the keys and values like so:
-#
-# key: foo, value: bar
-# key: baz, value: 12
-#
-# Note: Google "python keyword arguments".
+# # Write a function f4 that accepts an arbitrary number of keyword arguments and
+# # prints out the keys and values like so:
+# #
+# # key: foo, value: bar
+# # key: baz, value: 12
+# #
+# # Note: Google "python keyword arguments".
 
-# Your code here
+# # Your code here
 
 
 
-# Should print
-# key: a, value: 12
-# key: b, value: 30
-f4(a=12, b=30)
+# # Should print
+# # key: a, value: 12
+# # key: b, value: 30
+# f4(a=12, b=30)
 
-# Should print
-# key: city, value: Berkeley
-# key: population, value: 121240
-# key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+# # Should print
+# # key: city, value: Berkeley
+# # key: population, value: 121240
+# # key: founded, value: "March 23, 1868"
+# f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+# d = {
+#     "monster": "goblin",
+#     "hp": 3
+# }
 
-# What thing do you have to add to make this work?
-f4(d)
-# Your code here
+# # What thing do you have to add to make this work?
+# f4(d)
+# # Your code here
 
